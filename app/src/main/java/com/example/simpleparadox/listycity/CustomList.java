44371fe4f598f01448e43c2c543a3b9cf18f71ef
Aaron Zhang <bolun1@ualberta.ca>
2021-11-01 14:07:56 -0600

    RED: delete functionality won't work

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -54,4 +54,8 @@ public class CustomList extends ArrayAdapter<City> {
     public void addCity(City city){
         cities.add(city);
     }
+
+    public void deleteCity(City city){
+        cities.remove(city);
+    }
 }
