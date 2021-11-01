1a246792960db95b721d95f0a82ebb178081bbd2
Aaron Zhang <bolun1@ualberta.ca>
2021-11-01 14:50:20 -0600

    GREEN: count function now works

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -53,7 +53,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
     @Override
     public int getCount() {
-
+        return cities.size();
     }
     public void addCity(City city){
         cities.add(city);
